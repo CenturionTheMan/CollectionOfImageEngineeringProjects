6f3f03a2b5254a9647f9e11ec302e4bfc0d32cf1
CenturionTheMan <dziedziakmichal0@gmail.com>
2024-04-28 12:48:44 +0200

    lab3 update - code works?

---
+++ Lab3/helpers.py
@@ -0,0 +1,211 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from matplotlib import pyplot
+import numpy as np
+from scipy.fftpack import dct
+from scipy.fftpack import idct
+import struct
+import zlib
+
+
+def plot(data, title, rows, columns, index):
+    plt.subplot(rows, columns, index)
+    plt.imshow(data)
+    plt.gray()
+    plt.axis('off')
+    plt.title(title)
+    #plt.tight_layout(pad=1, w_pad=0.8, h_pad=0.5)
+
+def dct2(array):
+    return dct(dct(array, axis=0, norm='ortho'), axis=1, norm='ortho')
+
+
+def idct2(array):
+    return idct(idct(array, axis=0, norm='ortho'), axis=1, norm='ortho')
+
+
+#
+# Calculate quantisation matrices
+#
+# Based on: https://www.hdm-stuttgart.de/~maucher/Python/MMCodecs/html/jpegUpToQuant.html
+#           #step-3-and-4-discrete-cosinus-transform-and-quantisation
+#
+_QY = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                   [12, 12, 14, 19, 26, 48, 60, 55],
+                   [14, 13, 16, 24, 40, 57, 69, 56],
+                   [14, 17, 22, 29, 51, 87, 80, 62],
+                   [18, 22, 37, 56, 68, 109, 103, 77],
+                   [24, 35, 55, 64, 81, 104, 113, 92],
+                   [49, 64, 78, 87, 103, 121, 120, 101],
+                   [72, 92, 95, 98, 112, 100, 103, 99]])
+
+_QC = np.array([[17, 18, 24, 47, 99, 99, 99, 99],
+                   [18, 21, 26, 66, 99, 99, 99, 99],
+                   [24, 26, 56, 99, 99, 99, 99, 99],
+                   [47, 66, 99, 99, 99, 99, 99, 99],
+                   [99, 99, 99, 99, 99, 99, 99, 99],
+                   [99, 99, 99, 99, 99, 99, 99, 99],
+                   [99, 99, 99, 99, 99, 99, 99, 99],
+                   [99, 99, 99, 99, 99, 99, 99, 99]])
+
+def convert_8x8_to_channel(blocks, width):
+    step = int(width / 8)
+    rows = []
+
+    for i in range(0, len(blocks), step):
+        row_blocks = blocks[i:i + step]
+        max_height = max([block.shape[0] for block in row_blocks])
+        max_width = max([block.shape[1] for block in row_blocks])
+
+        # Create a list to store padded blocks
+        padded_blocks = []
+
+        for block in row_blocks:
+            # Pad block to have the same width and height
+            pad_height = max_height - block.shape[0]
+            pad_width = max_width - block.shape[1]
+            padded_block = np.pad(block, ((0, pad_height), (0, pad_width)), 'constant', constant_values=0)
+            padded_blocks.append(padded_block)
+
+        # Concatenate padded blocks along axis 1 to form a row
+        row = np.concatenate(padded_blocks, axis=1)
+        rows.append(row)
+
+    # Concatenate rows along axis 0 to form the channel
+    channel = np.concatenate(rows, axis=0)
+
+    return channel
+
+def zig_zag(block_of_image) -> list:
+    # setup of helper variables
+    n = x = y = 0
+    zig_zag_vector = np.zeros(64, dtype=np.uint8)
+    # main loop
+    while n < 64 :
+        #moving left
+        while x > -1 and y <8:
+            zig_zag_vector[n] = block_of_image[x ,y]
+            x-=1
+            y+=1
+            n+=1
+        x+=1
+        if y == 8:
+            y-=1
+            x+=1
+        #moving right
+        while y > -1 and x <8:
+            zig_zag_vector[n] = block_of_image[x, y]
+            y-=1
+            x+=1
+            n+=1
+        y+=1
+        if x == 8:
+            x-=1
+            y+=1
+    return zig_zag_vector
+
+def get_8x8_blocks_from_list(image_array):
+    """
+    Extracts 8x8 pixel data from a 2-dimensional image list, padding with zeros if necessary.
+
+    Parameters:
+    image_list (list): The 2-dimensional list representing the image.
+
+    Returns:
+    list: A list of 8x8 lists containing pixel data.
+    """
+
+    height, width = image_array.shape
+
+    pixel_data_list = []
+
+    for i in range(0, height, 8):
+        for j in range(0, width, 8):
+            # Extract 8x8 pixel data, padding with zeros if necessary
+            block = np.zeros((8, 8), dtype=image_array.dtype)
+            for x in range(8):
+                for y in range(8):
+                    if i + x < height and j + y < width:
+                        block[x, y] = image_array[i + x, j + y]
+
+            pixel_data_list.append(block)
+
+    return pixel_data_list
+
+def reconstruct_image_from_blocks(blocks, height, width):
+    """
+    Reconstructs a 2-dimensional numpy array from a list of 8x8 pixel blocks.
+
+    Parameters:
+    blocks (list): A list of 8x8 numpy arrays containing pixel data.
+    height (int): The height of the original image.
+    width (int): The width of the original image.
+
+    Returns:
+    numpy.ndarray: A 2-dimensional numpy array representing the reconstructed image.
+    """
+    # Initialize an empty array to hold the reconstructed image
+    reconstructed_image = np.zeros((height, width), dtype=blocks[0].dtype)
+
+    block_idx = 0
+    for i in range(0, height, 8):
+        for j in range(0, width, 8):
+            block = blocks[block_idx]
+            for x in range(8):
+                for y in range(8):
+                    if i + x < height and j + y < width:
+                        reconstructed_image[i + x, j + y] = block[x, y]
+            block_idx += 1
+
+    # Move to the next block
+    block_idx += 1
+
+    return reconstructed_image
+
+def adjust_size(base_params, reference):
+    res = []
+    for base in base_params:
+        if base.shape[1] > reference.shape[1]:
+            diff = base.shape[1] - reference.shape[1]
+            base = base[:, :-diff]
+        res.append(base)
+    return tuple(res)
+
+def compress(Y, CR, CB):
+    concat = np.concatenate((Y.flatten(), CR.flatten(), CB.flatten()), axis=0)
+    compress = zlib.compress(concat)
+    return compress
+
+def downsample(image_data, sample_rate=2):
+    CR = image_data[0::sample_rate,0::sample_rate,1]
+    CB = image_data[0::sample_rate,0::sample_rate,2]
+    Y  = image_data[0::,0::,0]
+    return CR, CB, Y
+
+def upsample(chanel, sample_rate=2):
+    upsample = np.repeat(chanel, sample_rate, axis=0)
+    upsample = np.repeat(upsample, sample_rate, axis=1)
+    return upsample
+
+
+
+def _scale(QF):
+    if QF < 50 and QF >= 1:
+        scale = np.floor(5000 / QF)
+    elif QF < 100:
+        scale = 200 - 2 * QF
+    else:
+        raise ValueError('Quality Factor must be in the range [1..99]')
+
+    scale = scale / 100.0
+    return scale
+
+
+def QY(QF=85):
+    return _QY * _scale(QF)
+
+
+def QC(QF=85):
+    return _QC * _scale(QF)
+
--- Lab3/main.py
+++ Lab3/main.py
@@ -1,14 +1,152 @@
+import os.path
+
+import matplotlib.pyplot as plt
 import numpy as np
-import cv2 as cv
+import cv2 as cv2
+import helpers as help
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+image = None
 
 
-# Press the green button in the gutter to run the script.
+def exercise1():
+    global image
+    image_data = np.copy(image)
+    width = image_data.shape[1]
+    height = image_data.shape[0]
+    xxxx = image_data.shape[2]
+
+    # 1. Convert RGB to YCbCr
+    image_data = cv2.cvtColor(image_data, cv2.COLOR_RGB2YCrCb)
+
+    # 2. Downsampling on Cb and Cr channels
+    CR_0, CB_0, Y = help.downsample(image_data, sample_rate=1)
+    CR_2, CB_2, Y = help.downsample(image_data, sample_rate=2)
+    CR_4, CB_4, Y = help.downsample(image_data, sample_rate=4)
+
+    # 3. Produce 8x8 blocks
+    #blocks = (width * height) // 64
+    y_blocks = help.get_8x8_blocks_from_list(Y)
+
+    CR_0_blocks, CB_0_blocks = help.get_8x8_blocks_from_list(CR_0), help.get_8x8_blocks_from_list(CB_0)
+    CR_2_blocks, CB_2_blocks = help.get_8x8_blocks_from_list(CR_2), help.get_8x8_blocks_from_list(CB_2)
+    CR_4_blocks, CB_4_blocks = help.get_8x8_blocks_from_list(CR_4), help.get_8x8_blocks_from_list(CB_4)
+
+    # 4. Calculate DCT on each block
+    y_blocks = [help.dct2(x) for x in y_blocks]
+    CR_2_blocks, CB_2_blocks = [help.dct2(x) for x in CR_2_blocks], [help.dct2(x) for x in CB_2_blocks]
+
+    # 5. Divide each block by quantisation matrix
+    y_blocks = [np.round(block / help.QY()) for block in y_blocks]
+    CR_2_blocks, CB_2_blocks = [np.round(block / help.QC()) for block in CR_2_blocks], [np.round(block / help.QC()) for block in CB_2_blocks]
+
+    # 6. Round values in each block to integers
+    y_blocks = np.array(y_blocks, dtype=int)
+    CR_2_blocks, CB_2_blocks = np.array(CR_2_blocks, dtype=int), np.array(CB_2_blocks, dtype=int)
+
+
+    # 7. Zig-zag
+    Y_zig_zag_blocks = np.asarray([help.zig_zag(y_blocks[x]) for x in range(len(y_blocks))])
+
+    CR_0_zig_zag_blocks = np.asarray([help.zig_zag(CR_0_blocks[x]) for x in range(len(CR_0_blocks))])
+    CB_0_zig_zag_blocks = np.asarray([help.zig_zag(CB_0_blocks[x]) for x in range(len(CB_0_blocks))])
+
+    CR_2_zig_zag_blocks = np.asarray([help.zig_zag(CR_2_blocks[x]) for x in range(len(CR_2_blocks))])
+    CB_2_zig_zag_blocks = np.asarray([help.zig_zag(CB_2_blocks[x]) for x in range(len(CB_2_blocks))])
+
+    CR_4_zig_zag_blocks = np.asarray([help.zig_zag(CR_4_blocks[x]) for x in range(len(CR_4_blocks))])
+    CB_4_zig_zag_blocks = np.asarray([help.zig_zag(CB_4_blocks[x]) for x in range(len(CB_4_blocks))])
+
+    # 8. Flatten, concatenate, compress and calculate the size -- how many bytes?
+    compress_0 = help.compress(Y_zig_zag_blocks, CR_0_zig_zag_blocks, CB_0_zig_zag_blocks)
+    compress_2 = help.compress(Y_zig_zag_blocks, CR_2_zig_zag_blocks, CB_2_zig_zag_blocks)
+    compress_4 = help.compress(Y_zig_zag_blocks, CR_4_zig_zag_blocks, CB_4_zig_zag_blocks)
+
+    #with open('image_zad4_rate1.jpg', 'wb') as fh:
+    #    fh.write(compress_0)
+    #with open('image_zad4_rate2.jpg', 'wb') as fh:
+    #    fh.write(compress_2)
+    #with open('image_zad4_rate4.jpg', 'wb') as fh:
+    #    fh.write(compress_4)
+
+    print(f"Dlugosc bez probkowania: {round(len(str(compress_0)) / 1024, 2)} KB")
+    print(f"Dlugosc z probkowaniem co 2. element: {round(len(str(compress_2)) / 1024, 2)} KB")
+    print(f"Dlugosc z probkowaniem co 4. element: {round(len(str(compress_4)) / 1024, 2)} KB")
+
+    # 5'. Reverse division by quantisation matrix -- multiply
+    y_blocks = [np.round(block * help.QY()) for block in y_blocks]
+    CR_2_blocks, CB_2_blocks = [np.round(block * help.QC()) for block in CR_2_blocks], [np.round(block * help.QC()) for block in CB_2_blocks]
+
+    # 4'. Reverse DCT and round
+    y_blocks = [help.idct2(x) for x in y_blocks]
+    CR_2_blocks, CB_2_blocks = [help.idct2(x) for x in CR_2_blocks], [help.idct2(x) for x in CB_2_blocks]
+
+    # 3'. Combine 8x8 blocks to original image
+    Y = help.reconstruct_image_from_blocks(y_blocks, height, width)
+    CR_0 = help.reconstruct_image_from_blocks(CR_0_blocks, CR_0.shape[0], CR_0.shape[1])
+    CB_0 = help.reconstruct_image_from_blocks(CB_0_blocks, CB_0.shape[0], CB_0.shape[1])
+    CR_2 = help.reconstruct_image_from_blocks(CR_2_blocks, CR_2.shape[0], CR_2.shape[1])
+    CB_2 = help.reconstruct_image_from_blocks(CB_2_blocks, CB_2.shape[0], CB_2.shape[1])
+    CR_4 = help.reconstruct_image_from_blocks(CR_4_blocks, CR_4.shape[0], CR_4.shape[1])
+    CB_4 = help.reconstruct_image_from_blocks(CB_4_blocks, CB_4.shape[0], CB_4.shape[1])
+
+    # 2'. Upsampling on Cb and Cr channels
+    CR_0, CB_0 = help.upsample(CR_0, 1), help.upsample(CB_0, 1)
+    CR_0, CB_0 = help.adjust_size((CR_0, CB_0), Y)
+    CR_2, CB_2 = help.upsample(CR_2, 2), help.upsample(CB_2, 2)
+    CR_2, CB_2 = help.adjust_size((CR_2, CB_2), Y)
+    CR_4, CB_4 = help.upsample(CR_4, 4), help.upsample(CB_4, 4)
+    CR_4, CB_4 = help.adjust_size((CR_4, CB_4), Y)
+
+    # 1'. Convert YCbCr to RGB
+    image_0 = cv2.cvtColor(np.stack((Y.astype(np.uint8), CR_0.astype(np.uint8), CB_0.astype(np.uint8)), axis=-1), cv2.COLOR_YCrCb2RGB)
+    image_2 = cv2.cvtColor(np.stack((Y.astype(np.uint8), CR_2.astype(np.uint8), CB_2.astype(np.uint8)), axis=-1), cv2.COLOR_YCrCb2RGB)
+    image_4 = cv2.cvtColor(np.stack((Y.astype(np.uint8), CR_4.astype(np.uint8), CB_4.astype(np.uint8)), axis=-1), cv2.COLOR_YCrCb2RGB)
+
+    help.plot(image, "Oryginalny obraz (png)", 2, 2, 1)
+    help.plot(image_0, "Obraz bez próbkowania, \npo kompresi i dekompresji", 2, 2, 2)
+    help.plot(image_2, "Obraz z próbkowaniem co 2. element, \npo kompresi i dekompresji", 2, 2, 3)
+    help.plot(image_4, "Obraz z próbkowaniem co 4. element, \npo kompresi i dekompresji", 2, 2, 4)
+    plt.show()
+
+# zad 1
+def exercise2():
+    pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    path = './png_file.png'
+    while not os.path.isfile(path):
+        print(f'Nie znaleziono pliku {path}!')
+        path = input('Wskaz sciezke do pliku: ')
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    exercise1()
+    input()
+    user_input = ''
+    while True:
+        print('[0] - Wyjscie')
+        print('[1] - Zajęcia 2 > Zadanie 4')
+        print('[2] - Zajęcia 2 > Zadanie 5')
+        print(f'[3] - Wybierz sciezke do zdjecia (wybrane zdjecie: {path})')
+        user_input = input('Wybierz opcje: ')
+        user_input = user_input.replace(' ', '').replace('\n', ' ')
+        if user_input == '0':
+            break
+        elif user_input == '1':
+            print('Ładuje zadanie ...')
+            exercise1()
+        elif user_input == '2':
+            print('Ładuje zadanie ...')
+            exercise2()
+        elif user_input == '3':
+            path = input('Podaj sciezke: ')
+            if os.path.isfile(path):
+                image = cv2.imread(path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            else:
+                print('Plik nie istnieje!')
+        else:
+            print('Nie rozpoznano opcji')
+        print('\n')
